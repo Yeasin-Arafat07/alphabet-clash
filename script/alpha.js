dd7a9d9b98ca460276090787369f4bed0a5a8fc7
YeasinArafat07 <yeasinaraft.dev@gmail.com>
2024-02-15 03:04:13 +0600

    all css work done

---
+++ script/alpha.js
@@ -0,0 +1,14 @@
+// function play(){
+//     // hide the screen, add class hidden
+//     const homeScreen = document.getElementById('home-screen');
+//     homeScreen.classList.add('hidden');
+//     //show the playground
+//     const playGround = document.getElementById('play-ground');
+//     playGround.classList.remove('hidden');
+
+// }
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+}
